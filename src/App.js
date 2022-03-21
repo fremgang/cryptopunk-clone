c051a781cc04b292bd2ae09d6e5a17331d596b4b
pakavi <pakavi@protonmail.ch>
2022-03-21 16:45:58 +0200

    Added collection card component w/ styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 
 import Header from "./components/Header.js";
+import CollectionCard from "./components/CollectionCard.js";
 
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={1}
+        name={"CP Punk 1"}
+        traits={[{ value: 7 }]}
+        image="https://ipfs.thirdweb.com/ipfs/QmX5KmDnfBdHQhmCSnqsb4yp3dyHcY6dzd2f7L9NEgATtQ/0.jpg"
+      />
     </div>
   );
 }
